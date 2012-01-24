b50891152d574f69dca480aab0c8145ee72bd212
David Hara <hakuhen@gmail.com>
2012-01-24 14:54:44 -0800

    changed code structure to follow the javascript module pattern, including moving code into cloudpalette.js and removing old files.

--- app/public/js/cloudpalette.js
+++ app/public/js/cloudpalette.js
@@ -4,27 +4,100 @@
  * The file where the magic happens.
  */
 
-var CloudPalette = {
-  images: {},
-};
+var CloudPalette = (function () {
+  var CP = {},
+      // Image prototype.
+      Image = function (w, h) {
+        var layers = [],
+            history = [],
+            placeInHistory = 0,
+            width = w,
+            height = h;
 
-include.includeInit([
+        // Create the initial layer
+        // TODO: will eventually have to change this to pass in actually data for the second argument.
+        layers.push(new Layer("background", {}));
+            
+        // Image public functions
+        // TODO: Should these be added on to the prototype property after?
+        
+        this.getLayers = function () {
+          return layers;
+        };
+        
+        this.getLayer = function (i) {
+          if (layers[i]) {
+            return layers[i];
+          } else {
+            console.error ("No layer with index " + index + " exists.");
+          }
+        };
+        
+        this.newLayer = function (name, data) {
+          layers.push(new Layer(name, data));
+        };
+        
+        
+        //TODO: Might need to do object/array cloning in the following three functions
+        // because the history array is an array of layer objects. Look up the best way
+        // to clone arrays of objects.
+        this.recordHistory = function () {
+          placeInHistory++;
+          history[placeInHistory] = layers;          
+        }
+        
+        this.undo = function () {
+          placeInHistory--;
+          layers = history[placeInHistory];
+        }
+        
+        this.redo = function () {
+          placeInHistory++;
+          layers = history[placeInHistory];
+        }
+        
+      },
   
-  /****** Utils ******/
-  '/js/util/canvasUtil.js',
-  
-  /****** Prototypes ******/
-  '/js/prototype/layer.js',
-  '/js/prototype/image.js'
-  
-]);
+      // Layer prototype.
+      Layer = function (n, d) {
+        var name = n,
+            data = d;
 
-$(function () {
-  var canvasJQ = $('#demo'),
-      canvas = $('#demo').get(0),
-      ctx = canvas.getContext('2d'),
-      offsetLeft = $('#demo').offset().left;
-      offsetTop = $('#demo').offset().top;
+        this.getData = function () {
+          return data;
+        };
+        
+        this.setData = function (d) {
+          data = d;
+        }
+        
+        this.getName = function () {
+          return name;
+        };
+        
+        this.setName = function (n) {
+          name = n;
+        }
+      },
+      images = {};
+      
+  CP.newImage = function (name, width, height) {
+    images[name] = new Image(width, height);
+  };
+      
+  CP.getImages = function () {
+    return images;
+  };
   
-  CloudPalette.images.push(new CloudPalette.Image(ctx, canvas.width, canvas.height));
-});
+  CP.getImage = function (name) {
+    if (images[name]) {
+      return images[name];
+    } else {
+      console.error("No image with the name " + name + " exists.");
+    }
+  };
+  
+  
+  return CP
+  
+}());
