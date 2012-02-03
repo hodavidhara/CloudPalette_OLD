e81f1521f2a5bc0a2c62c6229036a6f529e165f7
David Hara <hakuhen@gmail.com>
2012-02-03 15:36:36 -0800

    clicking new image now adds a draggable window containing a canvas in the body of the editor

--- app/public/js/editor.js
+++ app/public/js/editor.js
@@ -2,7 +2,8 @@
 // front-end scripts for the CloudPalette editor;
 
 $(function () {
-  // simple event binding
+  // Binding for the menu
+  // hover bind to show submenu dropdowns.
   $(".horizontal-menu-item").hover(
     function () {
       $(this).children(".horizontal-submenu").css('display', 'block');
@@ -10,5 +11,38 @@ $(function () {
     function () {
       $(this).children(".horizontal-submenu").css('display', 'none');
     }
-  )
+  );
+  // click binding for all the submenu items.
+  $('#new-image').click(createNewImage);
+  
+  var createNewImage = function () {
+    var canvasName = prompt('What would you like to name your Image?', 'Untitled');
+    $('body').append(
+    '<div id="canvas-' + canvasName + '" class="canvas-window">' +
+      '<div class="window-menu">' +
+        '<button name="close" class="close">Close!</button>' +
+        '<p>'+canvasName+'</p>' +
+      '</div>' +
+      '<canvas width="400" height="400">Get a real browser!</canvas>' +
+    '</div>'
+    )
+    makeDraggable(canvasName);
+  }
+  
+  var makeDraggable = function (canvasName) {
+    $(".canvas-window#canvas-" + canvasName).draggable({ disabled: true });
+    $(".canvas-window#canvas-" + canvasName).children(".window-menu").mousedown(
+      function () {
+        $(".canvas-window#canvas-" + canvasName).draggable("option", "disabled", false)
+        .trigger('mousedown')
+        .css('cursor', 'move');
+      }
+    ).mouseup(
+      function () {
+        $(".canvas-window#canvas-" + canvasName).draggable( "option", "disabled", true )
+        .css('cursor', 'auto');
+      }
+    );
+  };
+  
 });
