2720646260041c4d2f31a0ae99bcf4879e1fa254
David Hara <hakuhen@gmail.com>
2012-02-11 15:26:25 -0800

    changed the port again for nodester

--- app/app.js
+++ app/app.js
@@ -37,5 +37,5 @@ app.get('/editor', function(req, res){
   res.render('editor', {});
 });
 
-app.listen(314539);
+app.listen(14539);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
